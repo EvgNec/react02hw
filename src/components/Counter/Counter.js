ba8e8ee7b36914ecc6f2e4dfb5a8eeb75e5cfab4
Evgeniy <evgnec@gmail.com>
2024-04-28 16:28:55 +0300

    add css

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -2,19 +2,33 @@ import React from 'react';
 import './Counter.css';
 
 class Counter extends React.Component {
+  //     constructor() {
+  //         super();
+  //         this.state = {
+  //             value: 5,
+  //         };
+  // }
+    
+  state = {
+    value: 5,
+    };
+    
+    handleIncrement = () => {
+        this.setState({ value:10})
+    };
+    handleDecrement = () => {  this.setState({ value:7})};
+    
   render() {
     return (
       <div className="Counter">
-        <span className="Counter__value">0</span>
+        <span className="Counter__value">{this.state.value}</span>
         <div className="Counter__controls">
-                <button type="button" onClick={() => {
-                    console.log('Click button +');
-          }}>
+          <button type="button" onClick={this.handleIncrement}>
             Add 1
           </button>
-          <button type="button" onClick={() => {
-                    console.log('Click button -');
-          }}>Minus 1</button>
+          <button type="button" onClick={this.handleDecrement}>
+            Minus 1
+          </button>
         </div>
       </div>
     );
