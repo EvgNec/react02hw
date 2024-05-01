d5c15cc667083db5d3f66dfe3b1a1b717a3c5b81
Evgeniy <evgnec@gmail.com>
2024-05-02 00:13:52 +0300

    upd

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import TodoList from './TodoList';
-// import ColorPicker from './ColorPicker';
-// import Counter from './Counter';
-// import Dropdown from './Dropdown';
+import ColorPicker from './ColorPicker';
+import Counter from './Counter';
+import Dropdown from './Dropdown';
 
-// const colorPickerOptions = [
-//   { label: 'red', color: '#F44336' },
-//   { label: 'green', color: '#4CAF50' },
-//   { label: 'blue', color: '#2196F3' },
-//   { label: 'grey', color: '#607D8B' },
-//   { label: 'pink', color: '#E91E63' },
-//   { label: 'indigo', color: '#3F51B5' },
-// ];
+const colorPickerOptions = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3' },
+  { label: 'grey', color: '#607D8B' },
+  { label: 'pink', color: '#E91E63' },
+  { label: 'indigo', color: '#3F51B5' },
+];
 
 class App extends Component {
   state = {
@@ -40,9 +40,9 @@ class App extends Component {
     return (
       <>
         <h1>State component</h1>
-        {/* <Counter initialValue={10} /> 
+        <Counter initialValue={10} /> 
       <Dropdown />
-      <ColorPicker options={colorPickerOptions} /> */}
+      <ColorPicker options={colorPickerOptions} />
         <div>
           <p>All {todos.length }</p>
           <p>Run {completedTodos}</p>
